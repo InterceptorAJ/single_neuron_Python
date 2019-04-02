9d11e6d91fc992c9c28703192880439d3179ad93
Arkadiusz Juszczak <44204536+InterceptorAJ@users.noreply.github.com>
2019-04-02 10:47:58 +0200

    Update task1.py

--- task1.py
+++ task1.py
@@ -1,5 +1,8 @@
+import matplotlib
 import numpy as np
 import random as random
+import matplotlib.pyplot as plt
+
 
 inputN = list(map(float, input("Podaj wejścia(dowolne liczby oddzielona spacją): ").split()))
 inputN = np.array(inputN)
@@ -11,30 +14,52 @@ def Count(inputN):
         counter = counter + 1
     return counter
 N = Count(inputN)
-z = sum(inputN) - 1
+z = sum(inputN) + 100
 z = np.array(z)
 K = int(input("Podaj liczbę epok: "))
 ten = float(input("Podaj skok treningowy: "))
 print("Liczba i rodzaj wejść: ", inputN)
 print ("Ustawiona liczba epok: ", K)
-interval1 = float(input("Podaj pierwszą początkową liczbę dla wagi od 0 do 1 (liczba dziesiętna z kropką): "))
-interval2 = float(input("Podaj ostatnią początkową liczbę dla wagi od 0 do 1 (liczba dziesiętna z kropką): "))
+interval1 = float(input("Podaj pierwszą liczbę dla zakresu początkowego dla wag: "))
+interval2 = float(input("Podaj drugą liczbę dla zakresu początkowego dla wag: "))
 neuralW = np.random.uniform(low=interval1, high=interval2, size=N)
-neuralW = np.array(neuralW)
+neuralW = np.array(np.double(neuralW))
 print ("Początkowe wagi: ")
 print (neuralW)
 k = 0
+learn_list = []
 for iteration in range(K):
     if k < K:
         y = sum(inputN * neuralW)
-        neuralW = neuralW + ten * (z-y) * inputN
+        delta = (z - y)
+        neuralW = neuralW + ten * (delta) * inputN
         k += 1
+        learn_list.append(delta)
     else:
         StopIteration()
 print ("Końcowe wagi: ")
 print (neuralW)
 result = inputN * neuralW
 print("Wynik otrzymany: ")
-print(sum(result))
+z1 = sum(result)
+print(z1)
 print("Wynik rzeczywisty: ")
 print(z)
+print("Różnica: ")
+diff = z - z1
+print(diff)
+print("Różnica w %: ")
+diff1 = (z - z1)/z * 100
+print(diff1)
+
+x = []
+for i in range(K):
+    x.append(i)
+y = np.array(learn_list)
+plt.plot(x, y)
+plt.xlabel('Epoki')
+plt.ylabel('Błąd')
+plt.title('Relacja nauczania (zmniejszanie błędu) do czasu (epoki)')
+plt.grid(True)
+plt.savefig('zad1.png')
+plt.show()
